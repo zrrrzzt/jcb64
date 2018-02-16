6897a2a0d2dc5d80e9d41b6db7e7d2f7efc0ee9e
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-02-16 14:56:17 +0100

    Makes minifiers happy (patch)

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const pako = require('pako')
 const Buffer = require('buffer/').Buffer
 
-module.exports.pack = json => {
+module.exports.pack = function (json) {
   const str = pako.deflate(JSON.stringify(json), {to: 'string'})
   const buf = Buffer.from(str)
   return buf.toString('base64')
 }
 
-module.exports.unpack = b64 => {
+module.exports.unpack = function (b64) {
   const buf = Buffer.from(b64.toString('base64'), 'base64')
   const json = JSON.parse(pako.inflate(buf.toString(), { to: 'string' }))
   return json
